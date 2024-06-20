f3cf26956f1e01d84b495e0cc4eca540030f11ff
Djahid <hnfdjahid@gmail.com>
2024-06-20 02:57:53 +0100

    added api for flights search

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Air Voyage
 
 ## Description
-A prototype of a travel webapp called Air Voyage, where the user can find and manege his flights, stays and plan trips in one place.
+A prototype of a travel webapp, where the user can find flights, stays and plan trips in one place.
 
 ## Languages and Frameworks
 + Express.js
 + Bootstrap
 
 ## Host locally
-To host locally go to the 'sever.js' file and change the `port` variable to an open port on your machine. Run the file using node `node server.js`. then open your browser and type 'localhost:' followed with the open port you entered.
+To host locally first install the dependencies using the terminal type `npm install`. Then go to the 'sever.js' file and change the `port` variable to an open port on your machine. Run the file using node `node server.js`. then open your browser and type 'localhost:' followed with the open port you entered.
 
 ![Screenshot of code showing where to enter the open port.](./public/resources/localhost-img.png)
 
 ## Features
 + User can creat an account and login.
-+ Search for stays and explore more about the destination.
++ Search for stays and explore destinations.
 + Search for flights.
 + Keep track of flights.
 
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-import express from "express";
+import express, { json, response } from "express";
 import bodyParser from "body-parser";
+import { resolveInclude } from "ejs";
+import axios from "axios";
 
 const app = express();
-const port = 3100;
-let passwordConfirmed = false;
+const port = 3000;
+const apiURL = 'https://test.api.amadeus.com/v2';
+const token = '8a7sAbmTZ2j7ytccHSILOijx9o2w';
 
+
+let passwordConfirmed = false;
+let newAccount = {
+    email: '',
+    password: ''
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function passwordIdentical (req, res, next) {
     if (req.body['password'] === req.body['passwordConfirmation']) {
+        newAccount.email = req.body['Email'];
+        newAccount.password = req.body['password'];
         passwordConfirmed = true;
     } else {
         passwordConfirmed = false;
@@ -30,7 +41,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/signup', (req, res) => {
-    res.render('signup.ejs')
+    res.render('signup.ejs');
 })
 
 app.get('/login', (req, res) => {
@@ -47,4 +58,46 @@ app.post('/signup', (req, res) => {
 
 app.post('/login', (req, res) => {
     res.sendStatus(202);
+})
+
+
+
+
+//API
+
+//getting flights
+app.get("/flights", async (req, res) => {
+    const search = {
+        originLocationCode: req.query.departure,
+        destinationLocationCode: req.query.destination,
+        departureDate: req.query['dep-date'],
+        // returnDate: req.query['ret-date'] || null,
+        adults: 1,
+        nonStop: true,
+        max: 10
+    };
+    console.log('\n--------------');
+    console.log(search);
+    console.log('\n');
+    try {
+        const response = await axios.get(apiURL+"/shopping/flight-offers", {headers: {Authorization: `Bearer ${token}`}, params: search});
+        const result = response.data.data;
+        let flights = result.map(flight => {
+            flight = flight.itineraries[0].segments[0];
+            return {
+                depCode: flight.departure.iataCode,
+                depTime: flight.departure.at.replace('T', ' '),
+                ariCode: flight.arrival.iataCode,
+                ariTime: flight.arrival.at.replace('T', ' '),
+                durationtime: flight.duration.slice(2),
+                carrierCode: flight.carrierCode,
+                airCraftCode: flight.aircraft.code
+            }
+        })
+        
+        console.log(flights);
+        res.render('index.ejs', {flights: flights});
+    } catch (error) {
+        console.log("error");
+    }
 })
